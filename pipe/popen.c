8d3518452372fe25b5431cf3853fa612231fbc40
David Ranieri <50286622+davranfor@users.noreply.github.com>
2020-11-29 22:34:55 +0100

    Add files via upload

---
+++ pipe/popen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *popen(const char *, const char *);
+int pclose(FILE *);
+
+int main(void)
+{
+	char result[1024];
+	FILE *cmd;
+
+	cmd = popen("ls -l", "r");
+	if (cmd == NULL)
+    {
+		perror("popen");
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(result, sizeof(result), cmd))
+    {
+		printf("output: %s", result);
+	}
+	pclose(cmd);
+	return 0;
+}
+
