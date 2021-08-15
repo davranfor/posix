ccbf0f51fed41d4f0244e30025cbad31b01ed9e6
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-08-15 13:12:44 +0200

    Add files via upload

--- socketpair/execl/main.c
+++ socketpair/execl/main.c
@@ -14,10 +14,7 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    printf("fd[0] = %d\n", fd[0]);
-    printf("fd[1] = %d\n", fd[1]);
-
-    char args[2] = "";
+    char arg[2] = {0};
     int pid;
 
     if ((pid = fork()) == -1)
@@ -30,9 +27,9 @@ int main(void)
         puts("I'm, the child");
 
         close(fd[1]);
-        args[0] = (char)fd[0];
+        arg[0] = (char)fd[0];
 
-        execl("./child", "child", args, (char *)NULL);
+        execl("./child", "child", arg, (char *)NULL);
         perror("execl");
     }
     else
@@ -40,9 +37,9 @@ int main(void)
         puts("I'm, the parent");
 
         close(fd[0]);
-        args[0] = (char)fd[1];
+        arg[0] = (char)fd[1];
 
-        execl("./parent", "parent", args, (char *)NULL);
+        execl("./parent", "parent", arg, (char *)NULL);
         perror("execl");
     }
     return 0;
