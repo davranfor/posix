ca1b5a3dc4f54c2b9b994cecb8f6ea6f23098ee4
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-11-05 21:15:11 +0100

    Add files via upload

--- fork/fork.c
+++ fork/fork.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
@@ -16,38 +16,33 @@ int main(void)
     }
     else if (pid == 0)
     {
-        /* start of child process */
+        // start of child process
         printf("I'm the child\n");
-
         // Do the child stuff ...
         data = 1;
     }
     else
     {
-        /* start of parent process */
+        // start of parent process
         printf("I'm the parent\n");
-
         // Do the parent stuff ...
         data = 2;
 
         int status;
 
-        /* Wait for child process */
-        if ((pid = wait(&status)) == -1)
+        // Wait for child process
+        if (wait(&status) == -1)
         {
             perror("wait");
             exit(EXIT_FAILURE);
         }
         else
         {
-            printf("I'm again the parent\n");
+            printf("I'm again the parent, the child has finished\n");
         }
     }
     // Code executed from both the parent and the child
-    printf("Press intro ");
-    getchar();
     printf("The %s filled data with %d\n", pid != 0 ? "parent" : "child", data);
-    printf("Bye\n");
     return 0;
 }
 
