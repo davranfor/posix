b6a94faddf0de3c213d2a59015f991578f15aaaf
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-11-06 11:02:40 +0100

    Add files via upload

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -1,9 +1,9 @@
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(void)
 {
