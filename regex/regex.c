11610ed0b36044c83c55dd366019adfa6e5e3b19
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-12-15 18:57:45 +0100

    Add files via upload

---
+++ regex/regex.c
@@ -0,0 +1,43 @@
+/*
+gcc -Wall -Wextra -o regex regex.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+
+int main(void)
+{
+    regex_t regex;
+    int result;
+
+    // Compile regular expression
+    result = regcomp(&regex, "^a[[:alnum:]]", 0);
+    if (result != 0)
+    {
+        perror("recomp");
+        exit(EXIT_FAILURE);
+    }
+    // Execute regular expression
+    result = regexec(&regex, "abc", 0, NULL, 0);
+    if (result == 0)
+    {
+        puts("Match");
+    }
+    else if (result == REG_NOMATCH)
+    {
+        puts("No match");
+    }
+    else
+    {
+        char msg[100];
+
+        regerror(result, &regex, msg, sizeof(msg));
+        perror("regexp");
+        exit(EXIT_FAILURE);
+    }
+    // Free regular expression
+    regfree(&regex);
+    return 0;
+}
+
