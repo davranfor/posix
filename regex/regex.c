6c96505856ffe4812f442f5695a47761b4abbc5e
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-12-16 07:33:53 +0100

    Add files via upload

--- regex/regex.c
+++ regex/regex.c
@@ -1,5 +1,7 @@
 /*
 gcc -Wall -Wextra -o regex regex.c
+
+https://man7.org/linux/man-pages/man3/regcomp.3.html
 */
 
 #include <stdio.h>
@@ -9,32 +11,33 @@ gcc -Wall -Wextra -o regex regex.c
 int main(void)
 {
     regex_t regex;
+    char msg[100];
     int result;
 
     // Compile regular expression
     result = regcomp(&regex, "^a[[:alnum:]]", 0);
     if (result != 0)
     {
-        perror("regcomp");
-        exit(EXIT_FAILURE);
-    }
-    // Execute regular expression
-    result = regexec(&regex, "abc", 0, NULL, 0);
-    if (result == 0)
-    {
-        puts("Match");
-    }
-    else if (result == REG_NOMATCH)
-    {
-        puts("No match");
+        regerror(result, &regex, msg, sizeof(msg));
+        fprintf(stderr, "%s\n", msg);
     }
     else
     {
-        char msg[100];
-
-        regerror(result, &regex, msg, sizeof(msg));
-        perror("regexec");
-        exit(EXIT_FAILURE);
+        // Execute regular expression
+        result = regexec(&regex, "abc", 0, NULL, 0);
+        if (result == 0)
+        {
+            puts("Match");
+        }
+        else if (result == REG_NOMATCH)
+        {
+            puts("No match");
+        }
+        else
+        {
+            regerror(result, &regex, msg, sizeof(msg));
+            fprintf(stderr, "%s\n", msg);
+        }
     }
     // Free regular expression
     regfree(&regex);
