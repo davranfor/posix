a95532a517e69e3030e3c44fca99196152f2be7b
David Ranieri <50286622+davranfor@users.noreply.github.com>
2023-03-12 17:51:37 +0100

    Add files via upload

--- socket/epoll/client.py
+++ socket/epoll/client.py
@@ -6,30 +6,43 @@ PORT = 8888
 BUFFER_SIZE = 32768
 EOT = 0x04
 
-def handle(sock):
-    for data in sys.stdin:
-        data = data + chr(EOT)
+def handle_send(sock, data):
+    data = (data + chr(EOT)).encode()
+    sent = 0
+    while sent < len(data):
         try:
-            sock.sendall(data.encode())
+            size = sock.send(data[sent:])
         except socket.error as e: 
             print('Error sending data: %s' % e, file = sys.stderr)
-            sys.exit(1) 
-        data = b''
-        rcvd = 0
-        while True:
-            try:
-                data += sock.recv(BUFFER_SIZE)
-            except socket.error as e: 
-                print('Error receiving data: %s' % e, file = sys.stderr)
-                sys.exit(1)
-            size = len(data)
-            if size == rcvd:
-                return
-            if data[-1] == EOT:
-                data = data.decode()[:-1]
-                break
-            rcvd = size
-        print('Size: ' + str(len(data)) + ' | Server says: ' + data, end = '')
+            sys.exit(1)
+        if size == 0:
+            return False
+        sent += size
+    return True
+
+def handle_recv(sock):
+    data = b''
+    rcvd = 0
+    while True:
+        try:
+            data += sock.recv(BUFFER_SIZE)
+        except socket.error as e: 
+            print('Error receiving data: %s' % e, file = sys.stderr)
+            sys.exit(1)
+        size = len(data)
+        if size == rcvd:
+            return False
+        if data[-1] == EOT:
+            data = data.decode()[:-1]
+            break
+        rcvd = size
+    print('Size: ' + str(len(data)) + ' | Server says: ' + data, end = '')
+    return True
+
+def handle(sock):
+    for data in sys.stdin:
+        if not handle_send(sock, data) or not handle_recv(sock):
+            return
 
 def main():
     address = (HOST, PORT)
