8e8bda0a0ef17f940b8f98a85b9e867c74862f65
David Ranieri <50286622+davranfor@users.noreply.github.com>
2024-04-21 12:40:53 +0200

    Add files via upload

--- pthread/producer_consumer.c
+++ pthread/producer_consumer.c
@@ -11,64 +11,75 @@
     } while (0)
 
 static pthread_mutex_t mutex;
-static pthread_cond_t cond_empty;
-static pthread_cond_t cond_full;
+static pthread_cond_t empty;
+static pthread_cond_t full;
 
 #define BUFFER_SIZE 10
 
 static int buffer[BUFFER_SIZE];
-static int count = 0;
+static int size = 0;
 
-static void *producer(void *arg)
+static void *produce(void *arg)
 {
     (void)arg;
 
-    int size = 0, quit = 0;
+    int quit = 0;
 
     while (!quit)
     {
         pthread_call(mutex_lock, &mutex);
-        while (count == BUFFER_SIZE)
+        while (size == BUFFER_SIZE)
         {
             // Buffer is full, wait for consumer to consume items
-            pthread_call(cond_wait, &cond_full, &mutex);
+            pthread_call(cond_wait, &full, &mutex);
         }
+        while (size < BUFFER_SIZE)
+        {
+            int item = rand() % 100;
 
-        int item = rand() % 100;
-
-        buffer[size++ % BUFFER_SIZE] = item;
-        quit = item == 0;
-        count++;
-        printf("Produced: %d\n", item);
-        // Signal consumer that buffer is not empty anymore
-        pthread_call(cond_signal, &cond_empty);
+            buffer[size] = item;
+            printf("%02d) Produced: %d\n", size++, item);
+            if (item == 0)
+            {
+                quit = 1;
+                break;
+            }
+        }
+        // Signal consumer that buffer is full
+        pthread_call(cond_signal, &empty);
         pthread_call(mutex_unlock, &mutex);
     }
     pthread_exit(NULL);
 }
 
-static void *consumer(void *arg)
+static void *consume(void *arg)
 {
     (void)arg;
 
-    int size = 0, quit = 0;
+    int quit = 0;
 
     while (!quit)
     {
         pthread_call(mutex_lock, &mutex);
-        while (count == 0)
+        while (size == 0)
         {
             // Buffer is empty, wait for producer to produce items
-            pthread_call(cond_wait, &cond_empty, &mutex);
+            pthread_call(cond_wait, &empty, &mutex);
         }
+        for (int i = 0; i < size; i++)
+        {
+            int item = buffer[i];
 
-        int item = buffer[size++ % BUFFER_SIZE];
-
-        quit = item == 0;
-        count--;
-        printf("Consumed: %d\n", item);
-        // Signal producer that buffer is not full anymore
-        pthread_call(cond_signal, &cond_full);
+            printf("%02d) Consumed: %d\n", i, item);
+            if (item == 0)
+            {
+                quit = 1;
+                break;
+            }
+        }
+        size = 0;
+        // Signal producer that buffer is empty
+        pthread_call(cond_signal, &full);
         pthread_call(mutex_unlock, &mutex);
     }
     pthread_exit(NULL);
@@ -78,17 +89,17 @@ int main(void)
 {
     srand((unsigned)time(NULL));
 
-    pthread_t producer_thread, consumer_thread;
+    pthread_t producer, consumer;
 
     pthread_call(mutex_init, &mutex, NULL);
-    pthread_call(cond_init, &cond_empty, NULL);
-    pthread_call(cond_init, &cond_full, NULL);
-    pthread_call(create, &producer_thread, NULL, producer, NULL);
-    pthread_call(create, &consumer_thread, NULL, consumer, NULL);
-    pthread_call(join, producer_thread, NULL);
-    pthread_call(join, consumer_thread, NULL);
-    pthread_call(cond_destroy, &cond_empty);
-    pthread_call(cond_destroy, &cond_full);
+    pthread_call(cond_init, &empty, NULL);
+    pthread_call(cond_init, &full, NULL);
+    pthread_call(create, &producer, NULL, produce, NULL);
+    pthread_call(create, &consumer, NULL, consume, NULL);
+    pthread_call(join, producer, NULL);
+    pthread_call(join, consumer, NULL);
+    pthread_call(cond_destroy, &empty);
+    pthread_call(cond_destroy, &full);
     pthread_call(mutex_destroy, &mutex);
     return 0;
 }
