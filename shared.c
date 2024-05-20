60c81c2101eeacaebb5a8dac2e64241a9eb34edc
David Ranieri <50286622+davranfor@users.noreply.github.com>
2024-05-20 10:16:40 +0200

    Add files via upload

---
+++ shared.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include "shared.h"
+
+void pool_set(struct poolfd *pool, char *data, size_t size)
+{
+    if (pool->type == POOL_ALLOCATED)
+    {
+        free(pool->data);
+    }
+    pool->type = POOL_BUFFERED;
+    pool->data = data;
+    pool->size = size;
+}
+
+int pool_add(struct poolfd *pool, const char *data, size_t size)
+{
+    if (pool->type == POOL_BUFFERED)
+    {
+        pool->data = NULL;
+        pool->size = 0;
+    }
+    pool->type = POOL_ALLOCATED;
+
+    char *temp = realloc(pool->data, pool->size + size);
+
+    if (temp == NULL)
+    {
+        return 0;
+    }
+    pool->data = temp;
+    memcpy(pool->data + pool->size, data, size);
+    pool->size += size;
+    return 1;
+}
+
+void pool_sync(struct poolfd *pool, size_t sent)
+{
+    pool->sent += sent;
+}
+
+void pool_reset(struct poolfd *pool)
+{
+    if (pool->type == POOL_ALLOCATED)
+    {
+        free(pool->data);
+    }
+    pool->data = NULL;
+    pool->size = 0;
+    pool->sent = 0;
+    pool->type = 0;
+}
+
+int unblock(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+
+    if (flags == -1)
+    {
+        return -1;
+    }
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
---
+++ shared.h
@@ -0,0 +1,27 @@
+#ifndef SHARED_H
+#define SHARED_H
+
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 1234 
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 32768
+
+#define POOL_BUFFERED    1
+#define POOL_ALLOCATED   2
+
+struct poolfd
+{
+    char *data;
+    size_t size, sent;
+    unsigned type;
+};
+
+void pool_set(struct poolfd *, char *, size_t);
+int pool_add(struct poolfd *, const char *, size_t);
+void pool_sync(struct poolfd *, size_t);
+void pool_reset(struct poolfd *);
+
+int unblock(int);
+
+#endif
+
